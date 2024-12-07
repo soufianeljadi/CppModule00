fac656a13cd3550375db677c63922179260187f1
EL JADI Soufiane <eljadi.souf@gmail.com>
2024-12-07 17:30:00 +0100

    rcoments

--- ex01/Utils.hpp
+++ ex01/Utils.hpp
@@ -1,18 +1,13 @@
 
 #ifndef UTILS_HPP
-
 # define UTILS_HPP
 
 # include <iostream>
 # include "Contact.hpp"
 
-/* Returns a string with n spaces */
-std::string	add_spaces(int n);
-
-/* Ensures a string has at most 'max' chars, adding '.' when needed */
-std::string	wide(std::string str);
-
-/* Prints existing contacts with a pretty format */
 int			search_display(Contact contacts[8]);
+
+std::string	wide(std::string str);
+std::string	add_spaces(int n);
 
 #endif
